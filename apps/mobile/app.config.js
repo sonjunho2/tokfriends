aed630458d371c872b912431156fd6a3227da4a9
sonjunho2 <216651428+sonjunho2@users.noreply.github.com>
2026-09-11 14:53:42 +0900

    refactor: remove legacy Android toolchain overrides

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,46 +1,7 @@
 // app.config.js
 
-// gradle 속성 보조 함수들
-const ensureGradleProperty = (properties, name, value) => {
-  if (value == null) {
-    return;
-  }
-  const stringValue = String(value);
-  const existingProperty = properties.find(
-    (property) => property.type === 'property' && property.key === name,
-  );
-  if (existingProperty) {
-    existingProperty.value = stringValue;
-  } else {
-    properties.push({ type: 'property', key: name, value: stringValue });
-  }
-};
-
-const setGradlePluginVersion = (buildGradleContents, version) => {
-  if (!version) {
-    return buildGradleContents;
-  }
-  const dependencyPattern =
-    /classpath\(['"]com\.android\.tools\.build:gradle(?::[^'"]+)?['"]\)/;
-  if (!dependencyPattern.test(buildGradleContents)) {
-    return buildGradleContents;
-  }
-  return buildGradleContents.replace(
-    dependencyPattern,
-    `classpath('com.android.tools.build:gradle:${version}')`,
-  );
-};
-
-// 스낵 환경 여부 판별
-const isSnackEnvironment = () => {
-  const flag = process.env.SNACK_ENV;
-  return flag === '1' || flag === 'true';
-};
-
-
 module.exports = ({ config }) => {
   const resolvedConfig = config ?? {};
-  const snackEnvironment = isSnackEnvironment();
   // 플러그인 목록 복사
   const plugins = [...(resolvedConfig.plugins ?? [])];
   const requiredPlugins = [
@@ -59,57 +20,6 @@ module.exports = ({ config }) => {
     if (!alreadyConfigured) {
       plugins.push(plugin);
     }
-  }
-
-  // 스낵 환경이 아닌 경우에만 Android 빌드 속성 플러그인 로딩
-  if (!snackEnvironment) {
-    // 동적으로 @expo/config-plugins 로드 (Snack에서 모듈 없을 때 오류 방지)
-    const { withGradleProperties, withProjectBuildGradle } = require('expo/config-plugins');
-
-    // Android 빌드 속성 적용 함수
-    const withAndroidBuildProperties = (config, options = {}) => {
-      const { android = {} } = options;
-      let updatedConfig = withGradleProperties(config, (gradleConfig) => {
-        const { modResults } = gradleConfig;
-
-        [
-          ['android.compileSdkVersion', android.compileSdkVersion],
-          ['android.targetSdkVersion', android.targetSdkVersion],
-          ['android.minSdkVersion', android.minSdkVersion],
-          ['android.kotlinVersion', android.kotlinVersion],
-        ].forEach(([name, propertyValue]) => {
-          ensureGradleProperty(modResults, name, propertyValue);
-        });
-
-        return gradleConfig;
-      });
-
-      if (android.gradlePluginVersion) {
-        updatedConfig = withProjectBuildGradle(updatedConfig, (gradleConfig) => {
-          gradleConfig.modResults.contents = setGradlePluginVersion(
-            gradleConfig.modResults.contents,
-            android.gradlePluginVersion,
-          );
-          return gradleConfig;
-        });
-      }
-
-      return updatedConfig;
-    };
-
-    // Android 빌드 속성 플러그인 추가
-    plugins.push([
-      withAndroidBuildProperties,
-      {
-        android: {
-          kotlinVersion: '2.0.21',
-          gradlePluginVersion: '8.7.2',
-          compileSdkVersion: 35,
-          targetSdkVersion: 35,
-          minSdkVersion: 24,
-        },
-      },
-    ]);
   }
 
   return {
